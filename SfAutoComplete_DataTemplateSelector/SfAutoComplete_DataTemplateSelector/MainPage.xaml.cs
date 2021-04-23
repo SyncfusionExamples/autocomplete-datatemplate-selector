a06d8cbdcc8c34276ca6b0a6ac8ffcf12e82ec5c
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-04-23 16:32:30 +0530

    DataTemplate selector has been added

---
+++ SfAutoComplete_DataTemplateSelector/SfAutoComplete_DataTemplateSelector/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SfAutoComplete_DataTemplateSelector
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+    
+}
---
+++ SfAutoComplete_DataTemplateSelector/SfAutoComplete_DataTemplateSelector/ViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SfAutoComplete_DataTemplateSelector
+{
+    public class MobileDetailViewModel
+    {
+        public ObservableCollection<MobileDetail> MobileCollection { get; set; }
+        public MobileDetailViewModel()
+        {
+            this.MobileCollection = new ObservableCollection<MobileDetail>()
+            {
+                new MobileDetail () { Mobile="Samasung S8", IsAvaiableInStock=false },
+                new MobileDetail () { Mobile="Samasung S9", IsAvaiableInStock=true },
+                new MobileDetail () { Mobile="Samsung S10", IsAvaiableInStock=true },
+                new MobileDetail () { Mobile="Samsung S10 plus", IsAvaiableInStock=true },
+                new MobileDetail () { Mobile="iPhone 7", IsAvaiableInStock=true },
+                new MobileDetail () { Mobile="iPhone 8", IsAvaiableInStock=true },
+                new MobileDetail () { Mobile="iPhone X", IsAvaiableInStock=false },
+                new MobileDetail () { Mobile="iPhone XR", IsAvaiableInStock=false },
+                new MobileDetail () { Mobile="iPhone XS", IsAvaiableInStock=true },
+            };
+        }
+    }
+    public class DataTemplateSelectorViewModel : DataTemplateSelector
+    {
+        public DataTemplate DefaultTemplate { get; set; }
+        public DataTemplate SpecificDataTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var message = item as MobileDetail;
+            if (message.IsAvaiableInStock == null)
+                return null;
+            return message.IsAvaiableInStock == false ? SpecificDataTemplate : DefaultTemplate;
+        }
+
+    }
+}
